107e7fe49a1642b2b59edc0952fe38c89ad40f3c
Arthur Ngondo <ngondoarthur@gmail.com>
2017-10-31 10:58:16 +0300

    Add stub of the program

--- recipe_app.py
+++ recipe_app.py
@@ -11,9 +11,30 @@ Program features:
     * Delete recipes
 
 NOTE:
-    Data stored in File: recipe_app.db
+    Data stored in Database File: recipe_app.db
 
 '''
 
 __AUTHOR__ = 'Arthur Ngondo'
 
+import apsw         # SQLite3 Wrapper
+import string       # 
+import webbrowser   # For printing on a web-browser
+
+class CookBook(object):
+    '''Class holds recipe_app database routines.'''
+    
+    def __init__(self):
+        '''Initialization function.
+        Sets up attributes and global variables'''
+        pass
+    
+    def database_menu():
+        '''A loop that displays a list of options that the user 
+        can perform'''
+        cbk = CookBook() # Initialize the class
+
+    
+
+if __name__ == '__main__':
+    CookBook.database_menu()
